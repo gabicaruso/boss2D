53d6f08a528a54a8f165444b7bc11988eba3a6d8
Gabriela Caruso <gabriela.caruso@yahoo.com.br>
2021-04-08 17:06:57 -0300

    mudanca do tabuleiro para minigames funcionando parcialmente

--- boss2d/Assets/Board/Scripts/GameControl.cs
+++ boss2d/Assets/Board/Scripts/GameControl.cs
@@ -25,12 +25,44 @@ public class GameControl : MonoBehaviour
         {
             player1.GetComponent<FollowThePath>().moveAllowed = false;
             player1StartWaypoint = player1.GetComponent<FollowThePath>().waypointIndex - 1;
+
+            if (PlayerPrefs.GetInt("mini1") ==  1)
+            {
+                PlayerPrefs.SetInt("mini1", 2);
+                SceneManager.LoadScene("MinigameTest");
+            }
+            if (PlayerPrefs.GetInt("mini2") ==  1)
+            {
+                PlayerPrefs.SetInt("mini2", 2);
+                SceneManager.LoadScene("MinigameTest");
+            }
+            if (PlayerPrefs.GetInt("mini3") ==  1)
+            {
+                PlayerPrefs.SetInt("mini3", 2);
+                SceneManager.LoadScene("MinigameTest");
+            }
         }
 
         if (player1.GetComponent<FollowThePath>().waypointIndex == player1.GetComponent<FollowThePath>().waypoints.Length)
         {
-            SceneManager.LoadScene("EndGame");
+            if (PlayerPrefs.HasKey("waypointIndex"))
+            {
+                PlayerPrefs.GetInt("waypointIndex");
+            }
+            if (PlayerPrefs.HasKey("mini1"))
+            {
+                PlayerPrefs.GetInt("mini1");
+            }
+            if (PlayerPrefs.HasKey("mini2"))
+            {
+                PlayerPrefs.GetInt("mini2");
+            }
+            if (PlayerPrefs.HasKey("mini3"))
+            {
+                PlayerPrefs.GetInt("mini3");
+            }
             gameOver = true;
+            SceneManager.LoadScene("EndGame");
         }
     }
 

f6e9fcc4ae7209b9b341a46f0eff7e3d59f74df8
luizasilveira <38442561+luizasilveira@users.noreply.github.com>
2021-04-20 16:07:12 -0300

    mini2

---
+++ boss2d/Assets/Minigames/Minigame2/Scripts/GameController2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController2 : MonoBehaviour
+{
+    public static GameController2 instance;
+    public float startTime = 5.0f;
+    private float timer;
+    //public static GameController2 instance;
+    public Text timerText;
+
+    private int iTime;
+
+    
+    private int moveCount;
+
+    void Start()
+    {
+        if(instance == null){
+            instance = this;
+        }
+        timer = startTime;
+    }
+
+    void Update()
+    {
+        if(timer > 0){
+            Timer();
+        }
+        else{
+            EndGame();
+        }
+     
+    }
+
+    void Timer(){
+        timer -= Time.deltaTime;
+        iTime = (int)timer;
+        timerText.text = iTime.ToString();
+    }
+
+    public void EndGame(){
+        //GameControl.vidas--;
+        SceneManager.LoadScene("PerdeuMini2");
+    }
+
+    public void Winner(){
+       // GameControl.pontos += 10;
+        SceneManager.LoadScene("VenceuMini2");
+    }
+}
---
+++ boss2d/Assets/Minigames/Minigame2/Scripts/door.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+
+    GameController2 gc;
+     void OnTriggerEnter2D(Collider2D col)
+    {
+        GameController2.instance.Winner();
+        
+    }
+}

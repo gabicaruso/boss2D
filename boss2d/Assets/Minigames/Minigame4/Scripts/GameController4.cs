12f6d65016527e1cbb58551bb2292c92e5d4065b
luizasilveira <38442561+luizasilveira@users.noreply.github.com>
2021-04-23 10:35:59 -0300

    mini4 - falta endGame()

---
+++ boss2d/Assets/Minigames/Minigame4/Scripts/GameController4.cs
@@ -0,0 +1,41 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+// using UnityEngine.UI;
+
+// public class GameController4 : MonoBehaviour
+// {
+//     public float startTime = 40.0f;
+//     private float timer;
+//     public static GameController3 instance;
+//     public Text timeText;
+
+    
+//     private int moveCount;
+
+//     void Start()
+//     {
+//       timer = startTime;
+//     }
+
+//     void Update()
+//     {
+//         DetectInput();
+//     }
+
+//     // void timer(){
+//     //     timer = Time.deltaTime;
+//     //     timeText = timer.ToString();
+//     // }
+
+//     public void EndGame(){
+//         GameControl.vidas--;
+//         //SceneManager.LoadScene("PerdeuMini1");
+//     }
+
+//     public void Winner(){
+//         GameControl.pontos += 10;
+//         //SceneManager.LoadScene("VenceuMini1");
+//     }
+// }
---
+++ boss2d/Assets/Minigames/Minigame4/Scripts/player4.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+// [RequireComponent(typeof(Rigidbody2D))]
+public class player4 : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	float movement = 0f;
+    public float movementSpeed = 10f;
+    private static int score;
+    public int objective = 10;
+    public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+
+	void FixedUpdate()
+	{
+        movement = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector2(movement * movementSpeed, rb.velocity.y);
+        score = book.points;
+        scoreText.text = "Score: " + score.ToString() + "/" + objective.ToString();
+        if(score >= objective){
+            SceneManager.LoadScene("VenceuMini4");
+        }
+        
+	}
+}

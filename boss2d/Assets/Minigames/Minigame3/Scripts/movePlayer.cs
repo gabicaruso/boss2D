640bf1b87fc91e56d910629b662de05eb38dff3e
luizasilveira <38442561+luizasilveira@users.noreply.github.com>
2021-04-26 23:14:03 -0300

    mini3

---
+++ boss2d/Assets/Minigames/Minigame3/Scripts/movePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class movePlayer : MonoBehaviour
+{
+    public float velocidade = 5.0f;
+    private Rigidbody2D myBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"),0,0) * Time.deltaTime * velocidade;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == "baditem"){
+            SceneManager.LoadScene("PerdeuMini3");
+            Destroy(collision.gameObject);  
+        }
+
+    }
+
+}
---
+++ boss2d/Assets/Minigames/Minigame3/Scripts/spawnItens.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnItens : MonoBehaviour
+{
+    // public GameObject item1Prefab;
+    // public GameObject item2Prefab;
+    // public GameObject item3Prefab;
+    public GameObject player;
+    public GameObject badItem;
+
+    public List<GameObject> ItemsToSpawn;
+
+    // Start is called before the first frame update
+    public float SpawnTime;
+    //    public GameObject Object1;
+    //    public GameObject Object2;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(SpawnObject());
+    }
+
+    public IEnumerator SpawnObject() {
+        int rand = 0;
+        GameObject toSpawn;
+        rand = Random.Range(0, ItemsToSpawn.Count);
+        toSpawn = ItemsToSpawn[rand];
+        int i = Random.Range (0, 6);
+        Instantiate(toSpawn, new Vector3(Random.Range(-6f,6f),6,0), Quaternion.identity);
+        yield return new WaitForSeconds(SpawnTime);
+        Instantiate(badItem, new Vector3(Random.Range(-5.5f,5.5f),10,0), Quaternion.identity);
+        StartCoroutine(SpawnObject());
+    }
+
+    
+}
